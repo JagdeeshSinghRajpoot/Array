86a5165ab1be82593981b9d0dfa406f616d422b7
jagdeesh rajpoot <jagdeeshrajpoot9@gmail.com>
2023-08-05 11:10:08 +0530

    frist commit for ArrayBasic.java

---
+++ ArrayBasic.java
@@ -0,0 +1,22 @@
+// import java.util.Scanner;
+
+public class ArrayBasic {
+    public static void main(String[] args) {
+        int marks[] = new int[100];
+
+        System.out.println(marks.length);
+        // Scanner sc = new Scanner(System.in);
+        // marks[0] = sc.nextInt();
+        // marks[1] = sc.nextInt();
+        // marks[2] = sc.nextInt();
+
+        // System.out.println("phy : " + marks[0]);
+        // System.out.println("chem : " + marks[1]);
+        // marks[2] += 1;
+        // System.out.println("math : " + marks[2]);
+
+        // int percentage = (marks[0] + marks[1] + marks[2]) / 3;
+        // System.out.println("percentage : " + percentage);
+
+    }
+}
